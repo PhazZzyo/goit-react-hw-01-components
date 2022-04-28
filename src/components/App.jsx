95d0eaab6770592493cf5e12d96d3378d1b7c343
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-04-28 18:03:04 +0300

    Update App.jsx
    clear comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,6 @@ import user from './user.json';
 
 export const App = () => {
   return (
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     textTransform: 'uppercase',
-    //     color: '#010101',
-    //   }}
-    // >
-    //   React homework template
-
-    // </div>
-
     <div>
       <h1>React homework 01 - components</h1>
       <h2>Task 01 - Social network profile</h2>
