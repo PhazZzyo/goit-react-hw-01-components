1e7a80d8d7aed2cd03b44296f60e5d08b87c1fcb
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-04-28 17:31:03 +0300

    01-task
    Competed 01 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,34 @@
+import Profile from './social-profile/Profile';
+import './social-profile/Profile.css';
+import user from './user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     textTransform: 'uppercase',
+    //     color: '#010101',
+    //   }}
+    // >
+    //   React homework template
+
+    // </div>
+
+    <div>
+      <h1>React homework 01 - components</h1>
+      <h2>Task 01 - Social network profile</h2>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
